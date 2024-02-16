4231bf6b6f0bed4714292de7288c8ba68b6de0f1
Shubham Deshmukh <shubhamdeshm37@gmail.com>
2024-02-16 23:54:24 +0530

    Creating Class and it's object and constructor

--- my_app/lib/practise.dart
+++ my_app/lib/practise.dart
@@ -9,4 +9,15 @@ void main(){
   // for taking input from user
   var name = stdin.readLineSync();
   stdout.write("Welcome $name");
+
+
+  //let dsk =  new Human(); // creating a class object with new keyword
+  Human(); // creating a class object ; no need to use new keyword
+}
+
+class Human{
+
+//   constructor
+    Human();
+
 }
