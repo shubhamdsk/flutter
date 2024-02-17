38a3c8f4aac6adce5d601805c1ced85d9914cced
Shubham Deshmukh <shubhamdeshm37@gmail.com>
2024-02-17 22:51:09 +0530

    Difference between dart var and dynamic variable

---
+++ my_app/lib/dartVarvDynamic.dart
@@ -0,0 +1,17 @@
+import 'dart:io';
+
+void main() {
+  var name = 'shubham';
+  stdout.write(name);
+
+  // dynamic section;
+  
+  var section;
+  section = 'b'; //string
+
+  section = 17; //int
+
+  section = true; //boolean
+
+  stdout.write(section);
+}
