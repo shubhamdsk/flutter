d7e14fe4dce5c9db3ea7a163315a33fa12fb5b3e
Shubham Deshmukh <shubhamdeshm37@gmail.com>
2024-02-24 20:49:09 +0530

    Conditional Statements in Flutter

---
+++ my_app/lib/04_conditionalStmt.dart
@@ -0,0 +1,21 @@
+void main() {
+  print("Conditional Statement");
+  var a = 300;
+  var b = 50;
+
+  if (a > 200 || b < b) {
+    print('block 1 ');
+  } else {
+    print("block 2");
+  }
+
+  if (a > 100 && b > 50) {
+    print("Block 1");
+  } else if (a < 50) {
+    print("Block 2 ");
+  } else if (a > 80) {
+    print("Block 3");
+  } else {
+    print("Default");
+  }
+}
